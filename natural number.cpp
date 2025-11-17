a2ff94b3c11c5673ff806d9a65de3e2cfcf7f487
knishalini1497 <knishalini1497@gmail.com>
2025-11-17 19:32:16 +0530

    Add files via upload

---
+++ natural number.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	printf("enter the integer(n):");
+	scanf("%d",&n);
+	printf("multiplication table of %d:\n",n);
+	for(i=1;i<=10;i++)
+	{
+		printf("%d*%d=%d\n",n,i,n*i);
+	}
+}
+

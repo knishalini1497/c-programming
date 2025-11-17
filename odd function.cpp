bfc486f504c5a0ac1fe867d24e6020237701fb62
knishalini1497 <knishalini1497@gmail.com>
2025-11-17 19:32:50 +0530

    Add files via upload

---
+++ odd function.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+	int a;
+	printf("enter an integer:");
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+		printf("it is even");
+	
+	}
+	else
+	{
+		printf("it is odd");
+	}	
+	return 0;
+}

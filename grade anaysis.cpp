1099ba76db09f5de873b0521b5e544eef917a222
knishalini1497 <knishalini1497@gmail.com>
2025-11-17 19:30:19 +0530

    Add files via upload

---
+++ grade anaysis.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int x;
+	printf("enter your mark:");
+	scanf("%d",&x);
+	if(x>90)
+	{
+	printf("s grade");
+	}
+	else if("x>=80")
+	{
+	printf("a grade");
+	}
+	else if("x>=70")
+	{
+	printf("b grade");
+	}
+	else if("x>=60")
+	{
+	printf("c grade");
+	}
+	else
+	{
+	printf("fail");
+	}
+	return 0;
+}

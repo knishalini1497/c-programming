007af41ae32b2eb426d37b9e4b7920f75ec91cdb
knishalini1497 <knishalini1497@gmail.com>
2025-11-17 19:29:18 +0530

    Add files via upload

---
+++ even or odd.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int x;
+	printf("enter the value x:");
+	scanf("%d",&x);
+	if(x%2==0)
+	{
+	printf("the given value is even");
+	}
+	else
+	{
+	printf("the given value  is odd");
+	}
+	return 0;
+}
